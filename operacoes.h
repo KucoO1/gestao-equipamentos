8976a0b94199be76786f1842af39995275d95f3c
Emery-Lourenco <emerylourenco@gmail.com>
2025-04-25 21:45:40 +0100

    Operações com funcionalidade
    Falta dados para validações

---
+++ operacoes.h
@@ -0,0 +1,13 @@
+#ifndef OPERACOES_H
+#define OPERACOES_H
+
+
+void inserirOperacao();
+void alterarOperacao();
+void apagarOperacao();
+void listarOperacoes();
+void pesquisarOperacao();
+void salvarOperacoesEmArquivo();
+void carregarOperacoesDoArquivo();
+
+#endif
